41d6ad029359c23a7cbb88b7939b1831f029c8e5
Engy Magdy <engymelad237@gmail.com>
2026-09-24 05:20:09 +0300

    Implement high score tracking in Scoreboard
    Added high score functionality to the scoreboard, including methods to retrieve and save the high score.

--- score.py
+++ score.py
@@ -4,21 +4,39 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        self.highscore=self.get_highscore()
         self.color("white")
         self.penup()
         self.goto(0,350)
         self.hideturtle()
         self.update_score()
 
+    def get_highscore(self):
+        with open("highscore.txt") as file:
+            return int(file.read())
+        
+    def save_highscore(self):
+        with open("highscore.txt","w") as file:
+            file.write(str(self.highscore))
+
+
     def update_score(self):
-        self.write(f"Score: {self.score}",font=("arial",24,"normal"),align="center")
+        self.write(f"Score: {self.score}   High Score:{self.highscore}",font=("arial",24,"normal"),align="center")
+        
     def increase_score(self):
         self.score+=1
         self.clear()
         self.update_score()
-    def game_over(self):
+        
+  def game_over(self):
         window=Screen()
+        self.clear()
         window.bgcolor("darkred")
         self.goto(0,0)
+        if self.score>self.highscore:
+            self.highscore=self.score
+            self.save_highscore()
+        self.write(f"----------Game over----------\n\nYour Score:{self.score}\n\nHigh Score:{self.highscore}",font=("arial",30,"normal"),align="center")
+    
         self.write(f"Game over\nYour Score:{self.score}",font=("arial",30,"normal"),align="center")
     
